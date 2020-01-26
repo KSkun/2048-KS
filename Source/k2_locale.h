69e43e4ec92a0973aa42212c2b343373b37d2603
KSkun <ks@ksmeow.moe>
2020-01-27 01:41:14 +0800

    Tiny.

--- Source/k2_locale.h
+++ Source/k2_locale.h
@@ -13,5 +13,3 @@ extern const char *K2_UI_GAME_LOSE_DIALOG_TEXT;
 extern const char *K2_UI_HELP_MENU_ITEM_TEXT;
 extern const char *K2_UI_HELP_INFO_MENU_ITEM_TEXT;
 extern const char *K2_UI_INFO_DIALOG_TITLE;
-extern const char *K2_UI_INFO_DIALOG_TEXT_1;
-extern const char *K2_UI_INFO_DIALOG_TEXT_2;
